144b9c269ec9ece6455f9463fe2dce1fc8ba5746
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-06-23 16:14:50 +0200

    add post_list and post detail

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request,'posts/post_list.html',{})
+    posts=Post.objects.all()
+    context={'posts':posts}
+    return render(request,'posts/post_list.html',context)
 
 
 def post_detail(request,slug):
-    return render(request,'posts/post_detail.html',{})
+    post=Post.objects.get(slug=slug)
+    context={
+        'post':post
+    }
+    return render(request,'posts/post_detail.html',context)
 
